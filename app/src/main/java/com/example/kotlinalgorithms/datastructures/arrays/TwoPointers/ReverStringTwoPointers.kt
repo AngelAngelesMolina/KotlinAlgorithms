578f0ddfd45f47c7c91f1e5880439de187cb55eb
Angel Angeles Molina <82426196+AngelAngelesMolina@users.noreply.github.com>
2025-09-07 22:57:10 -0600

    ReverseStringTwoPointers

--- app/src/main/java/com/example/kotlinalgorithms/datastructures/arrays/TwoPointers/ReverStringTwoPointers.kt
+++ app/src/main/java/com/example/kotlinalgorithms/datastructures/arrays/TwoPointers/ReverStringTwoPointers.kt
@@ -14,5 +14,35 @@ Salida: ['o','l','l','e','h'].
  */
 
 fun main() {
+    val textToConvert = "hello"
 
+    println("NORMAL -> ${textToConvert.invertString()}")
+    println("TWO POINTERS -> ${textToConvert.invertTwoPointers()}")
+
+}
+
+fun String.invertTwoPointers(): String {
+    println("ORIGINAL CONTENT $this")
+
+    val data = this.toCharArray()
+    val maxVal = data.size - 1
+    for (i in 0 until data.size) {
+        if (i == maxVal - i) break
+        val aux = data[maxVal - i]
+        data[maxVal - i] = data[i]
+        data[i] = aux
+    }
+
+    return data.contentToString()
+}
+
+
+fun String.invertString(): String {
+    val currentStringLength = this.length - 1
+    println(currentStringLength)
+    var stringInverted = ""
+    for (i in currentStringLength downTo 0) {
+        stringInverted += this[i]
+    }
+    return stringInverted
 }
